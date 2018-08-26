12e8596a7eee7735587865c6fb879d02e4c2e288
Je-vivi <sa517205@mail.ustc.edu.cn>
2018-08-26 19:56:52 +0800

    a include test

---
+++ includeTest/main.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+void test_print();
+
+int main()
+{
+	test_print();
+	printf("main\n");
+}
---
+++ includeTest/reference.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+void test_print()
+{
+	printf("this is a reference\n");
+}
